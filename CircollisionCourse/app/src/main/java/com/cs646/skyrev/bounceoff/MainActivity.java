d74cdea1bd3d8cc9c237d3c9f520fe85877ca3ca
skyrev <akashrevankar121091@gmail.com>
2018-02-27 15:05:49 -0800

    Added Toast | Centered reset button
    Added toast to display instructions
    Centered reset button

--- CircollisionCourse/app/src/main/java/com/cs646/skyrev/bounceoff/MainActivity.java
+++ CircollisionCourse/app/src/main/java/com/cs646/skyrev/bounceoff/MainActivity.java
@@ -2,7 +2,10 @@ package com.cs646.skyrev.bounceoff;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
-
+        Toast toast = Toast.makeText(this, "Tap and hold to draw circle.\n" +
+                "Hold and swipe circle to set it in motion.", Toast.LENGTH_LONG);
+        TextView textView = toast.getView().findViewById(android.R.id.message);
+        if(textView != null)
+            textView.setGravity(Gravity.CENTER);
+        toast.show();
     }
 
     public void reset(View resetButton) {
